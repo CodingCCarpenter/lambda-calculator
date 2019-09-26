eecc1dc7b01767554164c3876a975c40d609c7d7
CodingCCarpenter <christineassists@aol.com>
2019-09-26 01:00:10 -0500

    add imported data to state in specials.ja

--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -6,8 +6,11 @@ import SpecialButton from './SpecialButton';
 
 //Import your array data to from the provided data file
 
-const Specials = () => {
+export default function Specials (props) {
   // STEP 2 - add the imported data to state
+  const [specialState, changeSpecialState] = useState(specials);
+
+  
 
   return (
     <div>
