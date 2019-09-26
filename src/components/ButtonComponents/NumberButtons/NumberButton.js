178937b23c4f1cb3a5bc569a78f8217fa423d9f7
CodingCCarpenter <christineassists@aol.com>
2019-09-26 01:03:15 -0500

    number button

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,12 @@
 import React from "react";
 
-const NumberButton = () => {
+export default function NumberButton (props) {
+
   return (
-    <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+    <button className = {props.className} onClick={props.onClick}>
+      {/* Display a button element rendering the data being passed down from the parent container on props */
+      props.number
+      }
+    </button>
   );
 };
